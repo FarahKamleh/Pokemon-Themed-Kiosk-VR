7a460bcf05a285c9833256ad52bcfce524462f9b
Silver Nhan <votrinhnhan1403@gmail.com>
2022-10-12 15:36:29 -0500

    Add scripting for healing station + rigid body

---
+++ Project 428.P2.Group 4/Assets/HealingStationScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealingStationScript : MonoBehaviour
+{
+    public GameObject HealingSpot;
+    public GameObject Eve;
+    public GameObject Evoled_Eve;
+    public ParticleSystem Particle_1;
+    void Start()
+    {
+        Particle_1.Stop();
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter(Collision collision) {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            if (HealingSpot.name == contact.thisCollider.name && (Eve.name == contact.otherCollider.name || Evoled_Eve.name == contact.otherCollider.name)) {
+                print("Healing Station Triggered");
+                swap();
+                Eve.transform.Rotate(0.0f, 0.0f, 0.0f, Space.Self);
+                Evoled_Eve.transform.Rotate(0.0f, 0.0f, 0.0f, Space.Self);
+                Particle_1.Play();
+                StartCoroutine(wait());
+            }
+        }
+    }
+    void swap() {
+        var temp_pos = Eve.transform.position;
+        Eve.transform.position = Evoled_Eve.transform.position;
+        Evoled_Eve.transform.position = temp_pos;
+    }
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(1);
+    }
+}
