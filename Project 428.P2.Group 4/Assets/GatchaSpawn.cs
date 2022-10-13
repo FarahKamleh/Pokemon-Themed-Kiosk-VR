0f3438c51228cc1ff25f2b4e713a9b72b5a01a87
Silver Nhan <votrinhnhan1403@gmail.com>
2022-10-13 11:12:45 -0500

    Update GatchaSpawn.cs

--- Project 428.P2.Group 4/Assets/GatchaSpawn.cs
+++ Project 428.P2.Group 4/Assets/GatchaSpawn.cs
@@ -8,15 +8,17 @@ public class GatchaSpawn : MonoBehaviour
     public GameObject Pokeball;
     public Vector3 poke_pos;
     public Quaternion poke_rotation;
-    void start() {
+    void Start() {
         poke_pos = Pokeball.transform.position;
         poke_rotation = Pokeball.transform.rotation;
-        Destroy(Pokeball);
+        poke_pos += new Vector3(0, 0, -0.7f);
+        Pokeball.transform.position = new Vector3(100, 100, 100);
     }
     void ButtonTouched() {
         // Button touched
         GameObject pokeB = Instantiate(Pokeball, poke_pos, poke_rotation);
         pokeB.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 20);
+        pokeB.transform.position += new Vector3(0, 0, 0.003f);
         print("pokeball spawned");
     }
 }
