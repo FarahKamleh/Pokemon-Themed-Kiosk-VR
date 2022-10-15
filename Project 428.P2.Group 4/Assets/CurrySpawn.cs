eecfd469ff87c210fb3116022ddda709c4fa82fd
FarahKamleh <fkamleh8800@gmail.com>
2022-10-14 19:05:52 -0500

    Curry Spawns
    When the microwave button is pressed, curry spawns.

--- Project 428.P2.Group 4/Assets/CurrySpawn.cs
+++ Project 428.P2.Group 4/Assets/CurrySpawn.cs
@@ -9,11 +9,25 @@ public class CurrySpawn : MonoBehaviour
     public GameObject Curry;
     public AudioSource Beep;
 
+    // for position, rotation, and scale
+    public Vector3 cPosition;
+    public Quaternion cRotation;
+    public Vector3 cScale;
+
     // Start is called before the first frame update
     void Start()
     {
         // disable beep at start
         Beep.Stop();
+
+        // copy the initial position, rotation, and scale of the curry in the scene
+        cPosition = Curry.transform.position;
+        cRotation = Curry.transform.rotation;
+        cScale = Curry.transform.localScale;
+
+        // move initial curry out of the way
+        Curry.transform.position = new Vector3(150, 150, 150);
+
     }
 
     // if collision occurs
@@ -21,5 +35,8 @@ public class CurrySpawn : MonoBehaviour
     {
         // when the button is pressed, make a beep
         Beep.Play();
+
+        // create new curry in microwave
+        GameObject SpawnedCurry = Instantiate(Curry, cPosition, cRotation);
     }
 }
