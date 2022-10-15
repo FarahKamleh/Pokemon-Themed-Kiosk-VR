ad068d14bbe60a1991028063d82833218e181c4f
FarahKamleh <fkamleh8800@gmail.com>
2022-10-14 20:13:28 -0500

    MakeHuman Sound Attempt
    Audio has been added for the Farah Makehuman, but the sound is not triggered when the user collides with the head.

---
+++ Project 428.P2.Group 4/Assets/FarahSound.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FarahSound : MonoBehaviour
+{
+    // the game objects involved
+    public GameObject Farah;
+    public AudioSource Vine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // disable sound at start
+        Vine.Stop();
+    }
+
+    // if collision occurs
+    void OnTriggerEvent(Collider collision)
+    {
+        // when head is touched, play sound
+        Vine.Play();
+    }
+}
